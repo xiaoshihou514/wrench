a3af39d63cff496d02c1869f492bb182dd483fe9
xiaoshihou514 <onelessononeexercise@gmail.com>
2024-06-18 11:34:20 +0100

    chore: use proper imports

--- src/wrench_build/wr.py
+++ src/wrench_build/wr.py
@@ -8,7 +8,7 @@ from parsy import ParseError, seq, string, regex
 
 from wrench_build.lib import info
 
-# from .lib import error, read_vars_till_invalild
+from .lib import error, read_vars_till_invalild, spaces
 
 help_str = """
 wrench
@@ -16,83 +16,6 @@ Run tasks defined in Wrenchfile
 Usage:
     wr [task1, task2, ...]
 """.strip()
-
-
-######################################################
-def error(msg: str):
-    print(f"\033[91m\033[1m{msg}\033[0m")
-
-
-spaces = regex(r"\s*")
-
-var_parser = seq(
-    name=regex(r"[^=\s]+") << spaces << string("="),
-    val=(spaces >> regex(".+")).map(
-        lambda args: list(
-            map(
-                lambda arg: (
-                    Var(arg[2:-1])
-                    if arg.startswith("$(") and arg.endswith(")")
-                    else arg
-                ),
-                args.split(),
-            )
-        )
-    ),
-)
-
-
-class Var:
-    name: str
-
-    def __init__(self, name: str):
-        self.name = name
-
-    @override
-    def __repr__(self):
-        return f"var({self.name})"
-
-
-def resolve_single(name: str, table: dict[str, list[str | Var]]):
-    val = table[name]
-    result: list[str] = []
-    for str_or_var in val:
-        if isinstance(str_or_var, str):
-            result.append(str_or_var)
-        else:
-            var = str_or_var.name
-            var_val = table.get(var)
-            if var_val is None:
-                error("{var} used but not declared")
-                exit(-1)
-            else:
-                resolve_single(var, table)
-                result += table[var]  # type: ignore[reportAttributeAccessIssue]
-    table[name] = result  # type: ignore[reportAttributeAccessIssue]
-
-
-def resolve(table: dict[str, list[str | Var]]):
-    for name in table:
-        if all(map(lambda sv: sv is str, table[name])):
-            pass
-        else:
-            resolve_single(name, table)
-
-
-def read_vars_till_invalild(lines: list[str]) -> dict[str, list[str]]:
-    lookup: dict[str, list[str | Var]] = {}
-    while True:
-        line = lines.pop(0).strip()
-        if not line:
-            continue
-        try:
-            key_val = var_parser.parse(line.strip())  # type: ignore[reportAny]
-            lookup[key_val["name"]] = key_val["val"]
-        except ParseError:
-            lines.insert(0, line)
-            break
-    resolve(lookup)
-    return lookup  # type: ignore[reportReturnType]
 
 
 decl_parser = seq(
@@ -112,9 +35,6 @@ def str_interpolate(line: str, lookup: dict[str, str]):
     return pattern.sub(
         lambda match: lookup.get(match.group(1), f"${{{match.group(1)}}}"), line
     )
-
-
-######################################################
 
 
 class Task:
